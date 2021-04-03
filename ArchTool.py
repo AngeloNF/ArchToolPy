068007ed1f363c297cce58f6fe55cb9af0ea5f6b
Angelo Nuñez Fonseca <djaanf@gmail.com>
2021-04-03 17:38:01 -0600

    Test 3

--- ArchTool.py
+++ ArchTool.py
@@ -1,3 +1,9 @@
-import os
+import subprocess
 
-os.system("ls -l /mnt")
+print("Bienvenido a la guia de instalación rapida de Arch")
+print("Comprobando la modalidad de arranque...")
+uefi = subprocess.run('ls /sys/firmware/efi/efivars', shell=True, stdout=subprocess.PIPE)
+if uefi.stderr is None:
+    print("Modalidad de arranque UEFI")
+if uefi.stdout is None:
+    print("Modalidad de arranque BIOS")
