3f278e82e3aa4056028f1fc7ae45a73ff018d749
Angelo Nuñez Fonseca <djaanf@gmail.com>
2021-04-03 20:15:40 -0600

    Primer intento de zona horaria

--- ArchTool.py
+++ ArchTool.py
@@ -1,4 +1,7 @@
 import subprocess
+import os 
+import os.path
+
 
 
 print("Bienvenido a la guia de instalación rapida de Arch")
@@ -30,6 +33,37 @@ hostname = input("Digite el nombre del equipo: ")
 
 subprocess.run('arch-chroot /mnt echo '+hostname+' >> /mnt/etc/hostname', shell=True)
 
+resultado =[]
+for item in os.listdir("/mnt/usr/share/zoneinfo"):
+    if os.path.isdir("/mnt/usr/share/zoneinfo/"+item):
+        resultado.append(item)
 
+print("Configurando zona horaria")
+while True:
+    print("Eliga una region")
+    for item in resultado:
+        print(item)
+    region = input("Escriba el nombre de la region: ").capitalize()
+    
+    regionTest = subprocess.run('ls /mnt/usr/share/zoneinfo/'+region, shell=True, stdout=subprocess.PIPE)
+    if regionTest.stderr is None:
+        break
+    else:
+        print("Error al encontrar la region, verifiquelo e intente de nuevo")
 
+resultado =[]
+for item in os.listdir("/mnt/usr/share/zoneinfo/"+region):
+    if os.path.isdir("/mnt/usr/share/zoneinfo/"+region+"/"+item):
+        resultado.append(item)
 
+while True:
+    print("Eliga un region")
+    for item in resultado:
+        print(item)
+    pais = input("Escriba el nombre de la region: ").capitalize()
+    
+    paisTest = subprocess.run('ls /mnt/usr/share/zoneinfo/'+region+"/"+pais, shell=True, stdout=subprocess.PIPE)
+    if paisTest.stderr is None:
+        break
+    else:
+        print("Error al encontrar la region, verifiquelo e intente de nuevo")
---
+++ Pruebas.py
@@ -0,0 +1,15 @@
+import os 
+import os.path
+
+
+hola = "tEST"
+print(hola.capitalize())
+# resultado =[]
+
+# for item in os.listdir("C:"):
+#     if os.path.isdir("C:"+item):
+#         resultado.append(item)
+
+# print(resultado)
+# for item in range(0, len(resultado)):
+#     print(str(item)+". "+resultado[item])``
